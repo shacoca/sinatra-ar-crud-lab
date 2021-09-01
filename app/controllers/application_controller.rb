cdaf03884b1bc9632c85123e99e2ff2f6014ecf2
shacoca <shacoca@yahoo.com>
2021-08-31 18:34:29 -0700

    switch to solution branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,51 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.all.find{|a| a.id.to_s == params[:id]}
+    if @article
+      erb :show
+    else
+      redirect '/articles'
+    end
+  end  
+  
+  post '/articles' do
+    @article = Article.create({:title => params[:article][":title"], :content => params[:article][":content"]})
+    redirect "/articles/#{Article.last.id}"
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.all.find{|a| a.id.to_s == params[:id]}
+    
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.all.find{|a| a.id.to_s == params[:id]}
+    article[:title] = params[:article][":title"]
+    article[:content] = params[:article][":content"]
+    article.save
+    @article = article
+    erb :show
+  end
+
+
+  delete '/articles/:id' do
+    article = Article.all.find{|a| a.id.to_s == params[:id]}
+    article.delete
+    erb :index
+  end
+
+
 end
